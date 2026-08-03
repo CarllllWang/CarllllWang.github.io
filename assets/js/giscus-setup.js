ee1586fade6d0b26e3ed4d0afb54a936580efa98
CarllllWang <92378334+CarllllWang@users.noreply.github.com>
2026-08-03 07:09:43 +0000

    Deploying to gh-pages from @ CarllllWang/CarllllWang.github.io@3db0d0c2ff676aebc77f402762dbb9688c6cfab0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "CarllllWang/Personal-Resume",
+    "data-repo": "CarllllWang/CarllllWang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
